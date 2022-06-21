b254f924ec89b1381bd2ab9a680772e555a03994
박태훈(IT서비스본부/개발실/프론트엔드팀/프로) <fethpiao@emforce.co.kr>
2022-06-21 10:31:23 +0900

    graph

---
+++ src/lib/common/html2canvas.js
@@ -0,0 +1,40 @@
+import html2canvas from 'html2canvas'
+export function aDownload(url, fileName) {
+	const a = document.createElement('a')
+	a.href = url
+	a.target = '_blank'
+	a.download = fileName || ''
+	document.body.appendChild(a)
+	a.click()
+	a.remove()
+}
+// 링크 다운로드 구현
+export function fileDownload(data, fileName) {
+	console.log('## aDownload Call: ', data, ' fileName: ', fileName)
+	if (data) {
+		const url = window.URL.createObjectURL(data)
+		aDownload(url, fileName)
+		window.URL.revokeObjectURL(url)
+	}
+}
+/**
+ * 지정된 영역을 image/png 형식으로 다운로드 합니다.
+ * backgroundColor : null
+ *
+ * * 4/28 이슈대응 : not allowed to navigate top frame to data url base64
+ * (크롬 보안 업데이트로  base64 데이터 url 열지 못하여 이미지다운로드 실패 에러 발생
+ *	=> blob 변환 후 다운로드)
+ *
+ * @param targetElement 다운로드 영역
+ * @param fileName 파일명
+ */
+export async function downloadHtmlToImg(targetElement, fileName) {
+	const canvas =  await html2canvas(targetElement, {backgroundColor: null})
+	console.log(canvas)
+	const imageUrl = canvas.toDataURL('image/png')
+	const blob = await (await fetch(imageUrl)).blob();
+
+	fileDownload(blob, fileName)
+
+	return true
+}
--- src/view/VueVis.vue
+++ src/view/VueVis.vue
@@ -7,6 +7,7 @@
     </p>
     <DataInfo></DataInfo>
     <network
+        id="visNetwork"
         class="network"
         ref="network"
         :nodes="network.nodes"
@@ -14,51 +15,16 @@
         :options="network.options"
         @click="networkEvent('click')"
         @double-click="networkEvent('doubleClick')"
-        @oncontext="networkEvent('oncontext')"
         @hold="networkEvent('hold')"
-        @release="networkEvent('release')"
         @select="networkEvent('select')"
         @select-node="networkEvent('selectNode')"
         @select-edge="networkEvent('selectEdge')"
-        @deselect-node="networkEvent('deselectNode')"
-        @deselect-edge="networkEvent('deselectEdge')"
-        @drag-start="networkEvent('dragStart')"
-        @dragging="networkEvent('dragging')"
-        @drag-end="networkEvent('dragEnd')"
-        @hover-node="networkEvent('hoverNode')"
-        @blur-node="networkEvent('blurNode')"
-        @hover-edge="networkEvent('hoverEdge')"
-        @blur-edge="networkEvent('blurEdge')"
-        @zoom="networkEvent('zoom')"
-        @show-popup="networkEvent('showPopup')"
-        @hide-popup="networkEvent('hidePopup')"
-        @start-stabilizing="networkEvent('startStabilizing')"
-        @stabilization-progress="networkEvent('stabilizationProgress')"
-        @stabilization-iterations-done="
-        networkEvent('stabilizationIterationsDone')
-      "
-        @stabilized="networkEvent('stabilized')"
-        @resize="networkEvent('resize')"
-        @init-redraw="networkEvent('initRedraw')"
-        @before-drawing="networkEvent('beforeDrawing')"
-        @after-drawing="networkEvent('afterDrawing')"
-        @animation-finished="networkEvent('animationFinished')"
-        @config-change="networkEvent('configChange')"
-        @nodes-mounted="networkEvent('nodes-mounted')"
-        @nodes-add="networkEvent('nodes-add')"
-        @nodes-update="networkEvent('nodes-update')"
-        @nodes-remove="networkEvent('nodes-remove')"
-        @edges-mounted="networkEvent('edges-mounted')"
-        @edges-add="networkEvent('edges-add')"
-        @edges-update="networkEvent('edges-update')"
-        @edges-remove="networkEvent('edges-remove')"
     >
     </network>
-    <button @click="addNode">Add node</button>
-    <button @click="addEdge">Add edge</button>
-    <button @click="resetNetwork">Reset Network</button>
-    <button @click="removeNode">Remove Node</button>
-    <button @click="removeEdge">Remove Edge</button>
+    <div class="downloadBtn">
+      <button type="button" @click="downloadImage">Download</button>
+    </div>
+
     <div class="events">
       <p>
         Network events: <br />
@@ -73,20 +39,30 @@
 import DataInfo from "@/components/DataInfo";
 import { Network } from "@vue2vis/network";
 import "vis-network/styles/vis-network.css";
-const nodes = [
-  { id: '디자인', label: '디자인', value: 0.22137995638153773 },
-  { id: '제품', label: '제품', value: 0.06604884464492325 },
-  { id: '냉장고', label: '냉장고', value: 0.08333333333333333 },
-  { id: '스타일', label: '스타일', value: 0.08333333333333333 },
-  { id: '하나', label: '하나', value: 0.07636080536975727 },
-  { id: '생각', label: '생각', value: 0.08701303662200743 },
-  { id: '공간', label: '공간', value: 0.09411975018853684 },
-  { id: '사용', label: '사용', value: 0.09411975018853684 },
-  { id: '인테리어', label: '인테리어', value: 0.06604884464492324 },
-  { id: '다양', label: '다양', value: 0.039381539923353186 },
-  { id: '제작', label: '제작', value: 0.039381539923353186 },
-  { id: '시간', label: '시간', value: 0.0494792654464041 }
-]
+import {downloadHtmlToImg} from "@/lib/common/html2canvas";
+import node from "@/jsonData/node.json"
+import link from "@/jsonData/link.json"
+const nodeJson = node;
+const linkJson = link;
+
+const nodesParsed = Object.keys(nodeJson).map(key => {
+  return {
+    id: key,
+    label: key,
+    value: nodeJson[key]
+  }
+})
+console.log('nodesParsed', nodesParsed)
+
+const linkParsed = linkJson.map(link => {
+  return {
+    length: (link[0] * 100),
+    from: link[1].split(',')[0],
+    to: link[1].split(',')[1]
+  }
+})
+console.log('linkParsed', linkParsed)
+
 
 export default {
   name: "VueVis",
@@ -100,24 +76,23 @@ export default {
     networkEvents: "",
 
     network: {
-      nodes: nodes,
-      edges: [
-        { from: '디자인' ,to: '제품', length: 0.054665  * 100000 },
-        { from: '스타일' ,to: '냉장고', length: 0.049953  * 100000 },
-        { from: '하나' ,to: '생각', length: 0.049325  * 100000 },
-        { from: '공간' ,to: '디자인', length: 0.048068  * 100000 },
-        { from: '하나' ,to: '디자인', length: 0.046497  * 100000 },
-        { from: '사용' ,to: '디자인', length: 0.046183  * 100000 },
-        { from: '공간' ,to: '인테리어', length: 0.045869  * 100000 },
-        { from: '인테리어' ,to: '디자인', length: 0.043984  * 100000 },
-        { from: '다양' ,to: '디자인', length: 0.043041  * 100000 },
-        { from: '제작' ,to: '디자인', length: 0.041784  * 100000 },
-        { from: '사용' ,to: '제품', length: 0.041470  * 100000 },
-        { from: '공간' ,to: '사용', length: 0.041156  * 100000 },
-        { from: '시간' ,to: '생각', length: 0.040528 * 100000  }
-      ],
+      nodes: nodesParsed,
+      edges: linkParsed,
 
       options: {
+        physics: {
+
+          barnesHut: {
+            gravitationalConstant: -1000000,
+            springConstant: 0.001,
+            springLength: 200
+          }
+        },
+        interaction: {
+          tooltipDelay: 200,
+          hideEdgesOnDrag: true
+        },
+
 
         nodes: {
 
@@ -125,7 +100,7 @@ export default {
           scaling: {
             label: {
               enabled: true,
-              min: 50,
+              min: 20,
               max: 500
             }
           },
@@ -151,13 +126,6 @@ export default {
           // },
 
         },
-        // physics: {
-        //   barnesHut: {
-        //     "gravitationalConstant": -3900,
-        //     "centralGravity": 0
-        //   },
-        //   minVelocity: 1
-        // },
 
       }
     }
@@ -184,30 +152,13 @@ export default {
         to: this.nodes[n2].id
       });
     },
-    resetNetwork() {
-      this.nodes = [
-        { id: 1, label: "Node 1" },
-        { id: 2, label: "Node 2" },
-        { id: 3, label: "Node 3" },
-        { id: 4, label: "Node 4" },
-        { id: 5, label: "Node 5" }
-      ];
-      this.edges = [
-        { id: 1, from: 1, to: 3 },
-        { id: 2, from: 1, to: 2 },
-        { id: 3, from: 2, to: 4 },
-        { id: 4, from: 2, to: 5 },
-        { id: 5, from: 3, to: 3 }
-      ];
-      this.options = {};
-    },
-    removeNode() {
-      this.nodes.splice(0, 1);
-    },
-    removeEdge() {
-      this.edges.splice(0, 1);
+    async downloadImage() {
+      let elementById =document.querySelector('#visNetwork > div > canvas')
+      await downloadHtmlToImg(elementById,'chart')
+
     }
-  }
+  },
+
 
   }
 
@@ -223,8 +174,12 @@ export default {
   height: 70px;
 }
 .network {
-  height: 400px;
+  height: 80vh;
+  width: 80vw;
   border: 1px solid #ccc;
   margin: 5px 0;
+}
+.downloadBtn {
+  border: 1px solid blue;
 }
 </style>
